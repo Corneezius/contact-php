70b8dc1dc3b236d3ee0154ab95c99149dcee1c91
Corneezius <padma@email.comcfr2116@columbia.edu>
2016-09-09 15:23:12 -0400

    add root directory with twig template

--- app/app.php
+++ app/app.php
@@ -8,51 +8,14 @@
     }
 
     $app = new Silex\Application();
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
 ));
 
     $app->get("/", function() use ($app) {
 
-      $output = "";
-
-      $all_homies = Homie::getAll();
-
-        if (!empty($all_homies)) {
-            $output = $output . "
-                <h1>Homies</h1>
-                <p>Here are all your homies:</p>
-                ";
-
-            foreach ($all_homies as $homie) {
-                $output = $output . "<p>" . $homie->getName() . "</p>";
-            }
-
-        }
-
-
-      $output = $output . "
-        <form action='/homies' method='post'>
-            <label for='homie'>Homie</label>
-            <input id='name' name='name' type='text'>
-
-            <label for='homie'>Phone</label>
-            <input id='phone' name='phone' type='text'>
-
-            <label for='homie'>Address</label>
-            <input id='address' name='address' type='text'>
-
-            <button type='submit'>Add Homie</button>
-        </form>
-        ";
-
-        $output .= "
-            <form action='/delete_homies' method='post'>
-                <button type='submit'>Clear</button>
-            </form>
-        ";
-
-      return $app['twig']->render('homies.html.twig');
+      return $app['twig']->render('homies.html.twig', array('homies' => Homie::getAll()));
     });
 
     $app->post("/homies", function() {
